51fc4337c592f408d4fef367128c2d7b18a1bac6
nadzar mutaqin <solarislight80@gmail.com>
2022-02-01 17:45:46 +0700

    test & benchmark libraries

---
+++ libraries/jsonwebtoken/jwt.go
@@ -0,0 +1,68 @@
+package jsonwebtoken
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt"
+	config "github.com/smokers10/official-website.git/libraries"
+)
+
+type jwonWebToken struct {
+	Payload Payload
+}
+
+type Payload struct {
+	ID       int
+	Email    string
+	IsLogged bool
+}
+
+func JsonWebToken(payload *Payload) *jwonWebToken {
+	return &jwonWebToken{Payload: *payload}
+}
+
+func (j *jwonWebToken) Sign() (string, error) {
+	config := config.ReadConfig().Application
+
+	claims := jwt.MapClaims{
+		"ID":       j.Payload.ID,
+		"Email":    j.Payload.Email,
+		"IsLogged": j.Payload.IsLogged,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(config.Secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, err
+}
+
+func (j *jwonWebToken) Verificate(tokenString string) (*Payload, error) {
+	payload := j.Payload
+	config := config.ReadConfig().Application
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(config.Secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		payload.ID = int(claims["ID"].(float64))
+		payload.Email = claims["Email"].(string)
+		payload.IsLogged = claims["IsLogged"].(bool)
+
+		return &payload, nil
+	}
+
+	return nil, err
+}
---
+++ libraries/jsonwebtoken/jwt_test.go
@@ -0,0 +1,61 @@
+package jsonwebtoken
+
+import "testing"
+
+func TestSign(t *testing.T) {
+	payload := &Payload{
+		ID:       1,
+		Email:    "admin@admin.com",
+		IsLogged: true,
+	}
+
+	token, err := JsonWebToken(payload).Sign()
+	if err != nil {
+		t.Errorf("Sign FAILED \n")
+		t.Error(err)
+	}
+
+	if token == "" {
+		t.Errorf("Sign FAILED. Token is empty string! \n")
+	}
+
+	if token != "" {
+		t.Logf("Sign SUCCESS. Token value is %s \n", token)
+	}
+}
+
+func TestVerificate(t *testing.T) {
+	jwt := JsonWebToken(&Payload{})
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsIklEIjoxLCJJc0xvZ2dlZCI6dHJ1ZX0.O1d6ndbpW8XUMXOqADBB8lTL5fbMU2irlbaOMqaMByc"
+
+	claims, err := jwt.Verificate(token)
+	if err != nil {
+		t.Errorf("Sign FAILED \n")
+		t.Error(err)
+	}
+
+	if claims.Email == "admin@admin.com" && claims.ID == 1 && claims.IsLogged == true {
+		t.Logf("Verificate SUCCESS. The Payload is email = %s, id = %d, is logged = %t \n", claims.Email, claims.ID, claims.IsLogged)
+	} else {
+		t.Errorf("Varificate FAILED. The payload is not meet required payload \n")
+	}
+}
+
+func BenchmarkTestVerificate(b *testing.B) {
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsIklEIjoxLCJJc0xvZ2dlZCI6dHJ1ZX0.O1d6ndbpW8XUMXOqADBB8lTL5fbMU2irlbaOMqaMByc"
+	for i := 0; i < b.N; i++ {
+		JsonWebToken(&Payload{}).Verificate(token)
+	}
+}
+
+func BenchmarkSign(b *testing.B) {
+	payload := &Payload{
+		ID:       1,
+		Email:    "admin@admin.com",
+		IsLogged: true,
+	}
+
+	for i := 0; i < b.N; i++ {
+		JsonWebToken(payload).Sign()
+	}
+}
